622fa4ec932fd6dc5bc038de5bbca54e36d30ae5
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-26 23:50:42 +0530

    Create Profile Screen and Implement profile Activity

---
+++ app/src/main/java/com/ucscieee/madhack/agon/ui/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.ucscieee.madhack.agon.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.ucscieee.madhack.agon.R;
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_profile);
+    }
+}
