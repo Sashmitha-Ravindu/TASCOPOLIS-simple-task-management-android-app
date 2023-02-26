524c92cdbc1536eeaedb8f4b07520e5dfa543307
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-27 00:11:51 +0530

    Implement MainActivity.java

--- app/src/main/java/com/ucscieee/madhack/agon/ui/MainActivity.java
+++ app/src/main/java/com/ucscieee/madhack/agon/ui/MainActivity.java
@@ -3,9 +3,12 @@ package com.ucscieee.madhack.agon.ui;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ucscieee.madhack.agon.R;
 
@@ -17,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnProfile = findViewById(R.id.button4);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToProfileScreen();
+            }
+        });
     }
+
+    private void goToProfileScreen() {
+        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
 }
