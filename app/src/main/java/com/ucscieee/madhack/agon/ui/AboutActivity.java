6281c988c8b65fd6006e7263370138adc3e1dd7b
sashmithaR <sashmitharavindu33@gmail.com>
2023-02-26 23:55:10 +0530

    Implement AboutActivity.java

---
+++ app/src/main/java/com/ucscieee/madhack/agon/ui/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.ucscieee.madhack.agon.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.ucscieee.madhack.agon.R;
+
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
