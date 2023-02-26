58f1d6d052ae4ebe1b799cb297d07b07a4032188
Ravinduhash <rhatu2000@gmail.com>
2023-02-26 23:42:24 +0530

    Create About Screen

--- app/src/main/java/com/ucscieee/madhack/agon/ui/SignUpActivity.java
+++ app/src/main/java/com/ucscieee/madhack/agon/ui/SignUpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ucscieee.madhack.agon.R;
@@ -18,5 +19,31 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        TextView linkSignIn = findViewById(R.id.linkSignIn);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+
+        linkSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignInScreen();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainScreen();
+            }
+        });
+    }
+    private void goToSignInScreen() {
+        Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToMainScreen() {
+        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
